a45a80d32586c8f607545db355e443a107352c52
Tanvir123Pandher <99844063+Tanvir123Pandher@users.noreply.github.com>
2022-09-30 18:44:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+nosex=0
+nosey=0
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('PoseNet Is Intialized')
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+    fill(255,0,0)
+    stroke(255,0,0)
+   
+    image(clown_nose,nosex -50,nosey -15,100,100)
+
+}
+function take_snapshot(){
+    save("filter.png");
+}
+function gotPoses(results) {
+    if (results.length > 0)  
+    {
+       console.log(results);
+       console.log("nose x = " + results[0].pose.nose.x)
+       console.log("nose y = " + results[0].pose.nose.y)
+       nosex = results[0].pose.nose.x
+       nosey = results[0].pose.nose.y
+    }
+}
+
+function preload(){
+    clown_nose=loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
+}
+
+
+ 
